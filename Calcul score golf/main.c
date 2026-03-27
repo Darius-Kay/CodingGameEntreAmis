b7ac556b7777c6ea8b7dd62f8f02ebbcd8cc0d08
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2026-03-27 21:45:38 +0100

    easy

---
+++ Calcul score golf/main.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+	int par[9999];
+    int swing[9999];
+    int result = 0;
+
+    for (int i = 0; i < 18; i++) {
+        int x;
+        scanf("%d", &x);
+        par[i] = x;
+    }
+    for (int i = 0; i < 18; i++) {
+        int x;
+        scanf("%d", &x);
+        swing[i] = x;
+    }
+    int i = 0;
+    while (par[i] || swing[i])
+    {
+        result += swing[i] - par[i];
+        i++;
+    }
+    printf("%d", result);
+    return 0;
+}
